7a799eca3a205b7121147eebe7e1c6c847b4367b
cwoolf123 <chriswoolf@mail.com>
2021-01-30 00:51:20 +0000

    added the correct api call functionality for register and the urql crreateClient config for cors

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,16 +3,33 @@ import { Form, Formik } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { Box, Button } from '@chakra-ui/react';
+import { useMutation } from 'urql';
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
       return (
         <Wrapper variant="small">
           <Formik 
             initialValues={{ username: "", password: "" }}
             onSubmit={(values) => {
-                console.log(values);
+                return register(values);
             }}
           >
               {({ isSubmitting })=> (
@@ -21,7 +38,7 @@ const Register: React.FC<registerProps> = ({}) => {
                   <Box mt={4}>
                   <InputField name="password" placeholder="password" label="password" type="password" />
                   </Box>
-                  <Button mt={4} isLoading={isSubmitting} type="submit" variantColor="teal">register</Button>
+                  <Button mt={4} isLoading={isSubmitting} type="submit" variantcolor="teal">register</Button>
                 </Form>
               )}
           </Formik>
