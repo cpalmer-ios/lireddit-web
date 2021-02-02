a161ef07668ab96a5f24a2d533c5b40886f62a96
cwoolf123 <chriswoolf@mail.com>
2021-02-02 01:16:02 +0000

    added the username or email register and login functionality

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -16,9 +16,9 @@ const Register: React.FC<registerProps> = ({}) => {
       return (
         <Wrapper variant="small">
           <Formik 
-            initialValues={{ username: "", password: "" }}
+            initialValues={{ email: "", username: "", password: "" }}
             onSubmit={async (values, {setErrors}) => {
-                const response = await register(values);
+                const response = await register({ options: values });
               if(response.data?.register.errors) {
                 setErrors(toErrorMap(response.data.register.errors));
               } else if (response.data?.register.user) {
@@ -29,6 +29,9 @@ const Register: React.FC<registerProps> = ({}) => {
               {({ isSubmitting })=> (
                 <Form>
                   <InputField name="username" placeholder="username" label="username" />
+                  <Box mt={4}>
+                  <InputField name="email" placeholder="email" label="email" />
+                  </Box>
                   <Box mt={4}>
                   <InputField name="password" placeholder="password" label="password" type="password" />
                   </Box>
